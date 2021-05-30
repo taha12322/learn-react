977a99c2936e41c4228fc2bc3540d6ecd6e8fecb
taha12322 <66300435+taha12322@users.noreply.github.com>
2021-05-30 23:02:35 +0430

    render in dom and jsx

---
+++ learn/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './component/product/product'
+
+const App = () =>{
+    return (
+        <div id='main' className='container'>
+            <h1>react App</h1>
+            <Product />
+        </div>
+    )
+}
+
+export default App
---
+++ learn/src/component/product/product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Produect = () => {
+    return (
+        <div>
+            <h3>peoduct</h3>
+            <p>product name : book1</p>
+            <p>product price : 99</p>
+        </div>
+    )
+}
+
+export default Produect
